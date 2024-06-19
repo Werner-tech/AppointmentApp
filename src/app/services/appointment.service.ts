a4016fa828a8831a49eaba4ba0f9aa3057281b9f
Werner Odendaal <Werner@it-s.co.za>
2024-06-20 01:27:42 +0200

    grsfg

--- src/app/services/appointment.service.ts
+++ src/app/services/appointment.service.ts
@@ -8,7 +8,7 @@ import { Appointment } from '../models/Appointment.model';
 })
 export class AppointmentService {
   // private apiUrl = 'https://localhost:7003/api/Appointments';
-  private apiUrl = 'https://appointmentappapi.azure-api.net/api/appointments';
+  private apiUrl = 'https://appointmentapibackend.azurewebsites.net/api/Appointments';
 
   constructor(private http: HttpClient) { }
 
